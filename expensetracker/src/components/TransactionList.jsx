e16886a503c4d5572fc1e49458231d2a5f6b3e87
crypte-1 <renukavutukuri1@gmail.com>
2022-06-18 16:34:24 +0530

    .map rendering

--- expensetracker/src/components/TransactionList.jsx
+++ expensetracker/src/components/TransactionList.jsx
@@ -10,6 +10,14 @@ export const TransactionList = () => {
   return (
     <>
       <h3>History</h3>
+      <ul className="list">
+        {transactions.map((transaction) => (
+          <li className="minus">
+            {transaction.text} <span>-$400</span>
+            <button className="delete-btn">X</button>
+          </li>
+        ))}
+      </ul>
     </>
   );
 };
