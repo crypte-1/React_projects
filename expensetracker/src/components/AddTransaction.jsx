5c4f9ee0dfd756aeb68791f6562ebf0854fac5f3
crypte-1 <renukavutukuri1@gmail.com>
2022-06-19 12:19:50 +0530

    added AddTransaction

--- expensetracker/src/components/AddTransaction.jsx
+++ expensetracker/src/components/AddTransaction.jsx
@@ -1,12 +1,25 @@
-import React, { useState } from "react";
+import React, { useContext, useState } from "react";
+import { GlobalContext } from "../context/GlobalState";
 
 export const AddTransaction = () => {
   const [text, setText] = useState("");
   const [amount, setAmount] = useState(0);
+  const { addTransaction } = useContext(GlobalContext);
+  const onsubmit = (e) => {
+    e.preventDefault();
+
+    const newTransaction = {
+      id: Math.floor(Math.random() * 10000),
+      text,
+      amount: +amount,
+    };
+    // console.log(typeof newTransaction.amount); initially its a string so appending a '+' makes it a number
+    addTransaction(newTransaction);
+  };
   return (
     <>
       <h3>Add new transaction</h3>
-      <form id="form">
+      <form onSubmit={onsubmit} id="form">
         <div className="form-control">
           <label htmlFor="text">Text</label>
           <input
